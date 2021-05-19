11581cc6c16f17015f90d69374babcabd7786c74
iliakhlyzov <khlyzoff@gmail.com>
2021-05-19 20:18:29 +0300

    get longest length added

--- hexlet/js/arrays/get/getLongestLength.test.js
+++ hexlet/js/arrays/get/getLongestLength.test.js
@@ -1,14 +1,30 @@
-// @ts-check
-
-import getLongestLength from '../solution.js';
-
-test('longest length', () => {
-  expect(getLongestLength('jabjcdel')).toBe(7);
-  expect(getLongestLength('abcddef')).toBe(4);
-  expect(getLongestLength('abbccddeffg')).toBe(3);
-  expect(getLongestLength('abcd')).toBe(4);
-  expect(getLongestLength('1234561qweqwer')).toBe(9);
-  expect(getLongestLength('1234561qweqwerqer')).toBe(9);
-  expect(getLongestLength('')).toBe(0);
-});
-
+const getLongestLength = (str) => {
+  let sequence = [];
+  let maxLength = 0;
+ 
+  // Проходимся по всем символам переданной строки.
+  for (const char of str) {
+    // Ищем в сформированной последовательности
+    // позицию первого вхождения текущего символа.
+    const index = sequence.indexOf(char);
+    // Добавляем в последовательность текущий символ.
+    sequence.push(char);
+    if (index !== -1) {
+      // Если символ в последовательности был найден,
+      // значит в неё был добавлен повторяющийся символ.
+      // Отсекаем из все символы включая найденный.
+      sequence = sequence.slice(index + 1);
+    }
+    // Получаем длину последовательности.
+    const sequenceLength = sequence.length;
+    if (sequenceLength > maxLength) {
+      // Если длина последовательности больше чем максимальная,
+      // устанавливаем новую максимальную длину.
+      maxLength = sequenceLength;
+    }
+  }
+ 
+  return maxLength;
+};
+ 
+export default getLongestLength;
